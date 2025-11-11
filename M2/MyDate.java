31d35059c35f30fb508ed43c2d7b056bddec4728
hsneed5 <hsneed5@ivytech.edu>
2025-11-11 00:17:07 +0000

    finalized 11.1 assignment + GeometricObject.java

--- M2/MyDate.java
+++ M2/MyDate.java
@@ -2,10 +2,10 @@ import java.util.GregorianCalendar;
 
 public class MyDate {
     private int year;
-    private int month; // 0-based (0 = January)
+    private int month; 
     private int day;
 
-    // No-arg constructor that creates a MyDate object for the current date
+
     public MyDate() {
         GregorianCalendar calendar = new GregorianCalendar();
         this.year = calendar.get(GregorianCalendar.YEAR);
@@ -13,25 +13,24 @@ public class MyDate {
         this.day = calendar.get(GregorianCalendar.DAY_OF_MONTH);
     }
 
-    // Constructor with elapsed time since Jan 1, 1970
+
     public MyDate(long elapsedTime) {
         setDate(elapsedTime);
     }
 
-    // Constructor with specified year, month, and day
+
     public MyDate(int year, int month, int day) {
         this.year = year;
         this.month = month;
         this.day = day;
     }
 
-    // Getters
     public int getYear() {
         return year;
     }
 
     public int getMonth() {
-        // Return month as 1-based (January = 1)
+
         return month + 1;
     }
 
@@ -39,7 +38,7 @@ public class MyDate {
         return day;
     }
 
-    // Sets a new date for the object using elapsed time
+
     public void setDate(long elapsedTime) {
         GregorianCalendar calendar = new GregorianCalendar();
         calendar.setTimeInMillis(elapsedTime);
@@ -48,7 +47,6 @@ public class MyDate {
         this.day = calendar.get(GregorianCalendar.DAY_OF_MONTH);
     }
 
-    // Test program
     public static void main(String[] args) {
         MyDate date1 = new MyDate();
         MyDate date2 = new MyDate(34355555133101L);
